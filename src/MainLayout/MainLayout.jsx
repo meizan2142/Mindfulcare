858b6f6c097e689b8cf21fec9aacdd3cc4963b6e
Your Name01~git <saifsahai2002@gmail.com>
2025-03-21 22:42:57 +0600

    Navbar & Footer section are added

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router"
 import Navbar from "../SharedComponents/Navbar/Navbar"
-import Footer from "../SharedComponents/Navbar/Footer/Footer"
+import Footer from "../SharedComponents/Footer/Footer"
 
 const MainLayout = () => {
     return (
