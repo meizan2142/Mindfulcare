8afda561347236eb06187345dd51bb59219088b7
abujaher5 <abujaher5120@gmail.com>
2025-03-22 22:56:13 +0600

    sjfsd

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,20 +2,25 @@ import MainLayout from "../MainLayout/MainLayout";
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
 import { createBrowserRouter } from "react-router-dom";
+import Service from "../pages/Service/Service";
 
-export  const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <MainLayout />,
-        children: [
-            {
-                path: '/',
-                element: <Home />
-            },
-            {
-                path: '/about',
-                element: <About />
-            },
-        ]
-    }
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/service",
+        element: <Service />,
+      },
+    ],
+  },
 ]);
