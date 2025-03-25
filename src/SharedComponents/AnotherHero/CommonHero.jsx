5a247fa15e53178f9e69b5e2be8db3df97dfcb90
Your Name01~git <saifsahai2002@gmail.com>
2025-03-25 19:43:48 +0600

    set a dynamic heading & description for common banner

--- src/SharedComponents/AnotherHero/CommonHero.jsx
+++ src/SharedComponents/AnotherHero/CommonHero.jsx
@@ -1,4 +1,4 @@
-const CommonHero = () => {
+const CommonHero = ({heading, description}) => {
     return (
         <div className="flex flex-col items-start px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 py-12 sm:py-16 md:py-20 lg:py-24 bg-linear-to-r from-[#5EB47C] to-[#007D6E] bg-cover bg-center">
             <div className="flex gap-4 sm:gap-6 md:gap-8 lg:gap-10 flex-col sm:flex-row">
@@ -9,16 +9,13 @@ const CommonHero = () => {
                         style={{ fontFamily: 'Manrope' }}
                         className="text-3xl sm:text-4xl md:text-5xl lg:text-[68px] font-extrabold text-white"
                     >
-                        About Us
+                        {heading}
                     </h1>
                     <p
                         style={{ fontFamily: 'Poppins' }}
                         className="text-base sm:text-lg md:text-xl lg:text-[18px] font-normal text-white w-full sm:w-5/6 md:w-4/5 lg:w-2/3 leading-normal sm:leading-6"
                     >
-                        Lorem ipsum dolor sit amet consectetur. Convallis est urna adipiscing
-                        fringilla nulla diam lorem non mauris. Ultrices aliquet at quam
-                        adipiscing feugiat interdum mattis. Placerat donec risus diam sed et.
-                        A in ullamcorper ipsum.
+                        {description}
                     </p>
                 </div>
             </div>
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,13 @@ import CommonHero from "../../SharedComponents/AnotherHero/CommonHero";
 const About = () => {
   return (
     <div>
-      <CommonHero />
+      <CommonHero
+        heading={'About Us'}
+        description={`Lorem ipsum dolor sit amet consectetur. Convallis est urna adipiscing
+                        fringilla nulla diam lorem non mauris. Ultrices aliquet at quam
+                        adipiscing feugiat interdum mattis. Placerat donec risus diam sed et.
+                        A in ullamcorper ipsum.`}
+      />
     </div>
   );
 };
