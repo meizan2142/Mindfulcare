f49379e13069d38dfb50873497507e433b7e73b5
abujaher5 <abujaher5120@gmail.com>
2025-03-27 16:26:28 +0600

    added card and newsletter section on service.jsx

--- src/SharedComponents/Navbar/Navbar.jsx
+++ src/SharedComponents/Navbar/Navbar.jsx
@@ -2,33 +2,40 @@ import { NavLink } from "react-router";
 import { navLinks } from "./paths";
 
 const Navbar = () => {
-    const links = navLinks();
-    return (
-
-        <nav className="flex items-center justify-between bg-linear-to-r from-[#54AE7A] to-[#299574] px-4 py-2 text-white">
-            <div className="scale-100 rounded-2xl px-3 py-2 text-xl font-semibold text-white transition-all duration-200 hover:scale-110">
-                <h2>Mindfulcare</h2>
-            </div>
-            <ul className="flex items-center justify-between gap-6 text-slate-900">
-                {
-                    links.map((link, index) => <li key={index} className="cursor-pointer  rounded-full  font-normal px-6 py-2 text-white ">
-                        <NavLink
-                            to={link.path}
-                            className={({ isActive }) => (isActive ? 'text-[#EB744A] font-bold' : 'text-white font-normal')}
-                        >
-                            {link.pathName}
-                        </NavLink>
-                    </li>)
+  const links = navLinks();
+  return (
+    <div className="max-w-[1500px]">
+      <nav className="flex items-center justify-between bg-linear-to-r from-[#54AE7A] to-[#299574] px-4 py-2 text-white">
+        <div className="scale-100 rounded-2xl px-3 py-2 text-xl font-semibold text-white transition-all duration-200 hover:scale-110">
+          <h2>Mindfulcare</h2>
+        </div>
+        <ul className="md:flex items-center justify-between hidden gap-6 text-slate-900">
+          {links.map((link, index) => (
+            <li
+              key={index}
+              className="cursor-pointer  rounded-full  font-normal px-6 py-2 text-white "
+            >
+              <NavLink
+                to={link.path}
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-[#EB744A] font-bold"
+                    : "text-white font-normal"
                 }
-            </ul>
-            <div>
-                <li className="cursor-pointer flex gap-2 list-none rounded-full px-6 py-2 font-bold text-white bg-[#EB744A]">
-                    Contact Us
-                </li>
-            </div>
-        </nav>
+              >
+                {link.pathName}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <li className="cursor-pointer flex gap-2 list-none rounded-full px-6 py-2 font-bold text-white bg-[#EB744A]">
+            Contact Us
+          </li>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Navbar
+export default Navbar;
--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,10 +1,86 @@
 const Service = () => {
   return (
-    <div>
-      <p>Here is some service
-        Hey there, I&apos;m Saif.
-      </p>
-      <h1>This is service page.</h1>
+    <div className="max-w-[1500px]">
+      <div className="mx-auto text-center mt-2">
+        <h3 className="text-center mx-auto font-semibold ">Services</h3>
+        <h2 className="text-center font-bold p-2 w-1/2 mx-auto  text-4xl">
+          Empowering Minds Our Mental Health Consulting Services
+        </h2>
+      </div>
+      {/* card item */}
+
+      <div className=" grid md:grid-cols-2 lg:grid-cols-3 ">
+        <div className="mx-4 my-2 max-w-[340px] space-y-3 rounded-xl bg-white p-4 shadow-lg dark:bg-[#18181B]">
+          <div className="relative flex h-48 w-full justify-center lg:h-[260px]">
+            <img
+              width={400}
+              height={400}
+              className="rounded-lg bg-black/40 object-cover"
+              src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="card navigate ui"
+            />
+          </div>
+          <div className="space-y-4 font-semibold">
+            <h6 className="text-sm text-center md:text-base lg:text-lg">
+              Product Name
+            </h6>
+            <p className="text-xs font-semibold text-black-400 md:text-sm">
+              Waterproof Sport HD Monitor for MacBook
+            </p>
+          </div>
+          <div className="flex flex-wrap items-center justify-center gap-6 text-sm md:text-base">
+            <button className="rounded-lg bg-[#49B2FF] px-4 py-2 font-semibold text-white duration-300 hover:scale-105 hover:bg-sky-600">
+              See Details
+            </button>
+          </div>
+        </div>
+        <div className="mx-4 my-2 max-w-[340px] space-y-3 rounded-xl bg-white p-4 shadow-lg dark:bg-[#18181B]">
+          <div className="relative flex h-48 w-full justify-center lg:h-[260px]">
+            <img
+              width={400}
+              height={400}
+              className="rounded-lg bg-black/40 object-cover"
+              src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="card navigate ui"
+            />
+          </div>
+          <div className="space-y-4 font-semibold">
+            <h6 className="text-sm text-center md:text-base lg:text-lg">
+              Product Name
+            </h6>
+            <p className="text-xs font-semibold text-black-400 md:text-sm">
+              Waterproof Sport HD Monitor for MacBook
+            </p>
+          </div>
+          <div className="flex flex-wrap items-center justify-center gap-6 text-sm md:text-base">
+            <button className="rounded-lg bg-[#49B2FF] px-4 py-2 font-semibold text-white duration-300 hover:scale-105 hover:bg-sky-600">
+              See Details
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/*  Join to our newsletter */}
+      <div className="flex flex-col justify-center items-center my-4 space-y-4 bg-linear-to-r from-[#5EB47C] to-[#007D6E] h-[300px] w-full">
+        <div className="space-y-4 text-center mt-2 w-">
+          <h2 className="text-3xl font-bold ">Join To Our Newsletter</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
+            commodi?
+          </p>
+        </div>
+
+        <div className="p-3 justify-center mx-auto   text-white w-[150px] md:w-[400px] lg:w-[600px] flex">
+          <input
+            className="border-white  outline-none border border-r-0 p-0 md:p-0.5 lg:p-1 rounded-l-4xl flex-none lg:flex-1  "
+            type="text"
+            placeholder="Your Email"
+          />
+          <button className="p-0.5 text-[10px] md:text-[14px] lg:text-[16px] md:p-0.5 lg:p-1 rounded-r-4xl bg-[#EC744A]">
+            Subscribe Us
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
